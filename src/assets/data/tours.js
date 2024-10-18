023150a2deff453f5ec2cce1dfe0edeb770839c8
Keerthi657614 <keerthimittapalli0414@gmail.com>
2024-10-18 20:30:05 +0530

    first commit

---
+++ src/assets/data/tours.js
@@ -0,0 +1,820 @@
+
+// //  import tourImg01 from "../../assets/images/tour-img01.jpg";  
+// //  import tourImg02 from "../../assets/images/tour-img02.jpg";
+// //  import tourImg03 from "../../assets/images/tour-img03.jpg";
+// //  import tourImg04 from "../../assets/images/tour-img04.jpg";
+// //  import tourImg05 from "../../assets/images/tour-img05.jpg";
+// //  import tourImg06 from "../../assets/images/tour-img06.jpg";
+// //  import tourImg07 from "../../assets/images/tour-img07.jpg"; 
+
+// // const tours = [
+// //   {
+// //     id: "01",
+// //     title: "Westminister Bridge",
+// //     city: "London",
+// //     distance: 300,
+// //     price: 99,
+// //     maxGroupSize: 10,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg01,
+// //     featured: true,
+// //   },
+// //   {
+// //     id: "02",
+// //     title: "Bali, Indonesia",
+// //     city: "Indonesia",
+// //     distance: 400,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg02,
+// //     featured: true,
+// //   },
+// //   {
+// //     id: "03",
+// //     title: "Snowy Mountains, Thailand",
+// //     city: "Thailand",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg03,
+// //     featured: true,
+// //   },
+// //   {
+// //     id: "04",
+// //     title: "Beautiful Sunrise, Thailand",
+// //     city: "Thailand",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg04,
+// //     featured: true,
+// //   },
+// //   {
+// //     id: "05",
+// //     title: "Nusa Pendia Bali, Indonesia",
+// //     city: "Indonesia",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg05,
+// //     featured: false,
+// //   },
+// //   {
+// //     id: "06",
+// //     title: "Cherry Blossoms Spring",
+// //     city: "Japan",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg06,
+// //     featured: false,
+// //   },
+// //   {
+// //     id: "07",
+// //     title: "Holmen Lofoten",
+// //     city: "France",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg07,
+// //     featured: false,
+// //   },
+// //   {
+// //     id: "08",
+// //     title: "Snowy Mountains, Thailand",
+// //     city: "Thailand",
+// //     distance: 500,
+// //     price: 99,
+// //     maxGroupSize: 8,
+// //     desc: "this is the description",
+// //     reviews: [
+// //       {
+// //         name: "jhon doe",
+// //         rating: 4.6,
+// //       },
+// //     ],
+// //     avgRating: 4.5,
+// //     photo: tourImg03,
+// //     featured: false,
+// //   },
+// // ];
+
+// // export default tours;
+// import tourImg01 from "../images/tour-img01.jpg";
+// import tourImg02 from "../images/tour-img02.jpg";
+// import tourImg03 from "../images/tour-img03.jpg";
+// import tourImg04 from "../images/tour-img04.jpg";
+// import tourImg05 from "../images/tour-img05.jpg";
+// import tourImg06 from "../images/tour-img06.jpg";
+// import tourImg07 from "../images/tour-img07.jpg";
+
+// const tours = [
+//   {
+//     id: "01",
+//     title: "Westminister Bridge",
+//     city: "London",
+//     distance: 300,
+//     price: 99,
+//     maxGroupSize: 10,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg01,
+//     featured: true,
+//   },
+//   {
+//     id: "02",
+//     title: "Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 400,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg02,
+//     featured: true,
+//   },
+//   {
+//     id: "03",
+//     title: "Snowy Mountains, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg03,
+//     featured: true,
+//   },
+//   {
+//     id: "04",
+//     title: "Beautiful Sunrise, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg04,
+//     featured: true,
+//   },
+//   {
+//     id: "05",
+//     title: "Nusa Pendia Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg05,
+//     featured: false,
+//   },
+//   {
+//     id: "06",
+//     title: "Cherry Blossoms Spring",
+//     city: "Japan",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg06,
+//     featured: false,
+//   },
+//   {
+//     id: "07",
+//     title: "Holmen Lofoten",
+//     city: "France",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg07,
+//     featured: false,
+//   },
+//   {
+//     id: "08",
+//     title: "Snowy Mountains, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg03,
+//     featured: false,
+//   },
+// ];
+
+// export default tours;
+// import tourImg01 from "../images/tour-img01.jpg";
+// import tourImg02 from "../images/tour-img02.jpg";
+// import tourImg03 from "../images/tour-img03.jpg";
+// import tourImg04 from "../images/tour-img04.jpg";
+// import tourImg05 from "../images/tour-img05.jpg";
+// import tourImg06 from "../images/tour-img06.jpg";
+// import tourImg07 from "../images/tour-img07.jpg";
+
+// const tours = [
+//   {
+//     id: "01",
+//     title: "Westminister Bridge",
+//     city: "London",
+//     distance: 300,
+//     price: 99,
+//     maxGroupSize: 10,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg01,
+//     featured: true,
+//   },
+//   {
+//     id: "02",
+//     title: "Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 400,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg02,
+//     featured: true,
+//   },
+//   {
+//     id: "03",
+//     title: "Snowy Mountains, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg03,
+//     featured: true,
+//   },
+//   {
+//     id: "04",
+//     title: "Beautiful Sunrise, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg04,
+//     featured: true,
+//   },
+//   {
+//     id: "05",
+//     title: "Nusa Pendia Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg05,
+//     featured: false,
+//   },
+//   {
+//     id: "06",
+//     title: "Cherry Blossoms Spring",
+//     city: "Japan",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [],
+//     avgRating: 4.5,
+//     photo: tourImg06,
+//     featured: false,
+//   },
+//   {
+//     id: "07",
+//     title: "Holmen Lofoten",
+//     city: "France",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [],
+//     avgRating: 4.5,
+//     photo: tourImg07,
+//     featured: false,
+//   },
+//   {
+//     id: "08",
+//     title: "Snowy Mountains, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg03,
+//     featured: false,
+//   },
+// ];
+
+// export default tours;
+// import tourImg01 from "../images/tour-img01.jpg";
+// import tourImg02 from "../images/tour-img02.jpg";
+// import tourImg03 from "../images/tour-img03.jpg";
+// import tourImg04 from "../images/tour-img04.jpg";
+// import tourImg05 from "../images/tour-img05.jpg";
+// import tourImg06 from "../images/tour-img06.jpg";
+// import tourImg07 from "../images/tour-img07.jpg";
+// import tourImg08 from "../images/TajMahal.png";
+// import tourImg09 from "../images/kerala.png";
+
+// const tours = [
+//   {
+//     id: "01",
+//     title: "TajMahal, Agra",
+//     city: "India",
+//     distance: 400,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg08,
+//     featured: true,
+//   },
+//   {
+//     id: "02",
+//     title: "Kerala: For its Backwaters, Beaches & Culture",
+//     city: "India",
+//     distance: 300,
+//     address:'Somewhere in India',
+//     price: 99,
+//     maxGroupSize: 10,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//       {
+//         name: "jhon doe",
+//         rating: 5,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg09,
+//     featured: true,
+//   },
+//   {
+//     id: "03",
+//     title: "Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 400,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg02,
+//     featured: true,
+//   },
+//   {
+//     id: "04",
+//     title: "Snowy Mountains, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg03,
+//     featured: true,
+//   },
+//   {
+//     id: "05",
+//     title: "Beautiful Sunrise, Thailand",
+//     city: "Thailand",
+//     distance: 500,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg04,
+//     featured: true,
+//   },
+//   {
+//     id: "06",
+//     title: "Nusa Pendia Bali, Indonesia",
+//     city: "Indonesia",
+//     distance: 500,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg05,
+//     featured: false,
+//   },
+//   {
+//     id: "07",
+//     title: "Cherry Blossoms Spring",
+//     city: "Japan",
+//     distance: 500,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [
+//       {
+//         name: "jhon doe",
+//         rating: 4.6,
+//       },
+//     ],
+//     avgRating: 4.5,
+//     photo: tourImg06,
+//     featured: false,
+//   },
+//   {
+//     id: "08",
+//     title: "Holmen Lofoten",
+//     city: "France",
+//     distance: 500,
+//     address:'Somewhere',
+//     price: 99,
+//     maxGroupSize: 8,
+//     desc: "this is the description",
+//     reviews: [],
+//     avgRating: 4.5,
+//     photo: tourImg07,
+//     featured: false,
+//   },
+  
+  
+// ];
+
+// export default tours;
+import tourImg01 from "../images/TajMahal1.jpg";
+import tourImg08 from "../images/kerala.png";
+import tourImg09 from "../images/goa.png";
+import tourImg10 from "../images/sikkim1.png";
+import tourImg02 from "../images/tour-img02.jpg";
+import tourImg03 from "../images/tour-img03.jpg";
+import tourImg04 from "../images/tour-img04.jpg";
+import tourImg05 from "../images/tour-img05.jpg";
+import tourImg06 from "../images/tour-img06.jpg";
+import tourImg07 from "../images/tour-img07.jpg";
+const tours = [
+  {
+    id: "01",
+    title: "TajMahal",
+    city: "India",
+    distance: 300,
+    price: 99,
+    maxGroupSize: 10,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+      {
+        name: "jhon doe",
+        rating: 5,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg01,
+    featured: true,
+  },
+  {
+    id: "02",
+    title: "Goa",
+    city: "India",
+    distance: 400,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg09,
+    featured: true,
+  },
+  {
+    id: "03",
+    title: "Bali,Indonesia",
+    city: "Indonesia",
+    distance: 400,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg02,
+    featured: true,
+  },
+  {
+    id: "04",
+    title: "Kerala: For its Backwaters, Beaches & Culture",
+    city: "India",
+    distance: 400,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg08,
+    featured: true,
+  },
+  {
+    id: "05",
+    title: "Snowy Mountains, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg03,
+    featured: true,
+  },
+  {
+    id: "06",
+    title: "Beautiful Sunrise, Thailand",
+    city: "Thailand",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg04,
+    featured: true,
+  },
+  {
+    id: "07",
+    title: "Nusa Pendia Bali, Indonesia",
+    city: "Indonesia",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg05,
+    featured: false,
+  },
+  {
+    id: "08",
+    title: "Cherry Blossoms Spring",
+    city: "Japan",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg06,
+    featured: false,
+  },
+  {
+    id: "09",
+    title: "Holmen Lofoten",
+    city: "France",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [],
+    avgRating: 4.5,
+    photo: tourImg07,
+    featured: false,
+  },
+  {
+    id: "10",
+    title: "Sikkim: For its Breathtaking Natural Vistas and Enthralling Adventures",
+    city: "India",
+    distance: 500,
+    price: 99,
+    maxGroupSize: 8,
+    desc: "this is the description",
+    reviews: [
+      {
+        name: "jhon doe",
+        rating: 4.6,
+      },
+    ],
+    avgRating: 4.5,
+    photo: tourImg10,
+    featured: true,
+  },
+  
+];
+
+export default tours;
